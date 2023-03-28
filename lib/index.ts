a864fbc7e4e826cfed0836d852cac894d3309f0d
Nathanael Anderson <nathan@master-technology.com>
2023-03-27 19:16:46 -0600

    Update to allow dynamic loading to node module

--- lib/index.ts
+++ lib/index.ts
@@ -3,54 +3,64 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const windowregistry = process.platform === 'win32' ? require('./winregistry.node') : null;
+let windowsRegistry: any;
+export function setWindowsRegistry(nativeNodefile: any): void {
+	if (nativeNodefile != null) {
+		windowsRegistry = nativeNodefile;
+	}
+}
+
+export function loadWindowsRegistry(path='./winregistry.node'): boolean {
+	if (windowsRegistry == null) {
+		windowsRegistry = process.platform === 'win32' ? require(path) : null;
+	}
+	if (!windowsRegistry) {
+		console.error('Could not initialize Windows Registry native node module.');
+		return false;
+	}
+	return true;
+}
 
 export type HKEY = "HKEY_CURRENT_USER" | "HKEY_LOCAL_MACHINE" | "HKEY_CLASSES_ROOT" | "HKEY_USERS" | "HKEY_CURRENT_CONFIG";
 
 export function EnumRegKeyKeys(hive: HKEY, path: string) : string[] {
-	if (windowregistry) {
-		return windowregistry.EnumRegKeyKeys(hive, path);
-	} 
-	console.error('Could not initialize Windows Registry native node module.');
-	return [];
+	if (!windowsRegistry) {
+		if (!loadWindowsRegistry()) { return []; }
+	}
+	return windowsRegistry.EnumRegKeyKeys(hive, path);
 }
 
 export function EnumRegKeyValues(hive: HKEY, path: string) : string[] {
-	if (windowregistry) {
-		return windowregistry.EnumRegKeyValues(hive, path);
-	} 
-	console.error('Could not initialize Windows Registry native node module.');
-	return [];
+	if (!windowsRegistry) {
+		if (!loadWindowsRegistry()) { return []; }
+	}
+	return windowsRegistry.EnumRegKeyValues(hive, path);
 }
 
 export function GetStringRegKey(hive: HKEY, path: string, name: string): string | undefined {
-	if (windowregistry) {
-		return windowregistry.GetStringRegKey(hive, path, name);
+	if (!windowsRegistry) {
+		if (!loadWindowsRegistry()) { return undefined; }
 	}
-	console.error('Could not initialize Windows Registry native node module.');
-	return undefined;
+    return windowsRegistry.GetStringRegKey(hive, path, name);
 }
 
 export function SetStringRegKey(hive: HKEY, path: string, name: string, value: string) : void {
-	if (windowregistry) {
-		windowregistry.SetStringRegKey(hive, path, name, value);
-	} else {
-		console.error('Could not initialize Windows Registry native node module.');
+	if (!windowsRegistry) {
+		if (!loadWindowsRegistry()) { return; }
 	}
+	windowsRegistry.SetStringRegKey(hive, path, name, value);
 }
 
 export function DeleteRegKeyKey(hive: HKEY, path: string, name: string) : void {
-	if (windowregistry) {
-		windowregistry.DeleteRegKeyKey(hive, path, name);
-	} else {
-		console.error('Could not initialize Windows Registry native node module.');
+	if (!windowsRegistry) {
+		if (!loadWindowsRegistry()) { return; }
 	}
+    windowsRegistry.DeleteRegKeyKey(hive, path, name);
 }
 
 export function DeleteRegKeyValue(hive: HKEY, path: string, name: string) : void {
-	if (windowregistry) {
-		windowregistry.DeleteRegKeyValue(hive, path, name);
-	} else {
-		console.error('Could not initialize Windows Registry native node module.');
+	if (!windowsRegistry) {
+		if (!loadWindowsRegistry()) { return; }
 	}
+    windowsRegistry.DeleteRegKeyValue(hive, path, name);
 }
